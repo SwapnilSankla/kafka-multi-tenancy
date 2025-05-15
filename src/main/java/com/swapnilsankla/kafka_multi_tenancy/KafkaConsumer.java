9b27096c9cf44113bb4797d3f23d362dc8422a6c
Swapnil Sankla <swapnil.sankla@gmail.com>
2025-05-15 18:42:04 +0530

    Add retry

--- src/main/java/com/swapnilsankla/kafka_multi_tenancy/KafkaConsumer.java
+++ src/main/java/com/swapnilsankla/kafka_multi_tenancy/KafkaConsumer.java
@@ -1,12 +1,18 @@
 package com.swapnilsankla.kafka_multi_tenancy;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.retry.annotation.Backoff;
 
 import static org.springframework.kafka.support.KafkaHeaders.RECEIVED_TOPIC;
 
-public record KafkaConsumer(String topic, String groupId, ConcurrentKafkaListenerContainerFactory<String, String> factory) {
+public record KafkaConsumer(String topic, String groupId,
+                            ConcurrentKafkaListenerContainerFactory<String, String> factory) {
+  @RetryableTopic(
+    backoff = @Backoff(delay = 2000),
+    dltTopicSuffix = ".dlt")
   @KafkaListener(topics = "#{__listener.topic}", groupId = "#{__listener.groupId}", containerFactory = "#{__listener.factory}")
   public void listen(String message, @Header(RECEIVED_TOPIC) String topic) {
     System.out.println("Received Message on topic: " + topic + ", message: " + message);
